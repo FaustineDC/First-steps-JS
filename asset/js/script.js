d90b3b1a5cd2ed38fc1a8660afa5ba01ca0e0d9f
Faustine DC <faustine.drean-churie@live.fr>
2023-05-09 23:01:35 +0200

    creating html, css and js

---
+++ asset/js/script.js
@@ -0,0 +1,26 @@
+class Episode {
+    constructor(title, duration, hasBeenWatched) {
+      this.title = title;
+      this.duration = duration;
+      this.hasBeenWatched = hasBeenWatched;
+    }
+  }
+  
+  let firstEpisode = new Episode('Origins', '50', true);
+  let secondEpisode = new Episode('The Cave', '55', false);
+  let thirdEpisode = new Episode('Chapter 3', '50', false);
+
+  document.querySelector('#first-episode-info').innerText = 
+  `Episode: ${firstEpisode.title}
+  Duration: ${firstEpisode.duration} min
+  ${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+  document.querySelector('#second-episode-info').innerText = 
+  `Episode: ${secondEpisode.title}
+  Duration: ${secondEpisode.duration} min
+  ${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+  document.querySelector('#third-episode-info').innerText = 
+  `Episode: ${thirdEpisode.title}
+  Duration: ${thirdEpisode.duration} min
+  ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
